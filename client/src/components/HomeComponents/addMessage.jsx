e93230c9e7fcf88aab2e59c763169dcf0872dd65
imadbenmadi <imadbenmadi891@gmail.com>
2023-11-22 09:32:09 +0100

    .

--- client/src/components/HomeComponents/addMessage.jsx
+++ client/src/components/HomeComponents/addMessage.jsx
@@ -27,7 +27,7 @@ export default function AddMessage() {
                     "success"
                 );
                 setchanged(true);
-                 
+                
 
             }
         } catch (error) {
